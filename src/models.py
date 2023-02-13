86143c7828e9c21d7151a94088418d4edc4550d3
BIGOSCAR85 <rojas.osc85@gmail.com>
2023-02-13 01:32:13 +0000

    working on the vehicle

--- src/models.py
+++ src/models.py
@@ -4,7 +4,7 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
-from eralchemy import render_er
+from eralchemy2 import render_er
 
 Base = declarative_base()
 
@@ -45,7 +45,7 @@ class Planet(Base):
     diameter = Column(Integer)
     gravity = Column(String(250))
 
-    class Character(Base):
+class Character(Base):
     __tablename__ = 'character'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
@@ -55,6 +55,13 @@ class Planet(Base):
     eye_color = Column(String(250))
     skin_color = Column(String(250))
     
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    class_type = Column(String(250))
+    passenger = Column(Integer)
+    model = Column(String(250))
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
