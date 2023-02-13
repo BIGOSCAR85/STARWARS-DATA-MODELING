04724580bd77fabf6c0183ce3fb6d4f3a0653c8f
BIGOSCAR85 <rojas.osc85@gmail.com>
2023-02-13 00:33:49 +0000

    my first commit starwars data modeling

--- src/models.py
+++ src/models.py
@@ -4,30 +4,36 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
-from eralchemy2 import render_er
+from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    # class Person(Base):
+    #     __tablename__ = 'person'
+    #     # Here we define columns for the table person
+    #     # Notice that each column is also a normal Python instance attribute.
+    #     id = Column(Integer, primary_key=True)
+    #     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    # class Address(Base):
+    #     __tablename__ = 'address'
+    #     # Here we define columns for the table address.
+    #     # Notice that each column is also a normal Python instance attribute.
+    #     id = Column(Integer, primary_key=True)
+    #     street_name = Column(String(250))
+    #     street_number = Column(String(250))
+    #     post_code = Column(String(250), nullable=False)
+    #     person_id = Column(Integer, ForeignKey('person.id'))
+    #     person = relationship(Person)
 
-    def to_dict(self):
-        return {}
+    #     def to_dict(self):
+    #         return {}
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False, unique=True)
+    password = Column(String(250))
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
